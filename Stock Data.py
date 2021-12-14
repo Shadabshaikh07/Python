ce1b957e0625a09e9e69b515ba689b3fd04d86e1
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-12-14 14:15:48 +0100

    Assignment 3

--- Stock Data.py
+++ Stock Data.py
@@ -3,34 +3,26 @@ from urllib.request import urlopen
 
 def download_data(stock_symbol):
     url = 'https://query1.finance.yahoo.com/v7/finance/download/'+stock_symbol+'?period1=1587042293&period2=1618578293&interval=1d&events=history&includeAdjustedClose=true'
-
-    local_path = os.path.join('data', stock_symbol+'.csv')
+    file_name = stock_symbol+'.csv'
+    local_path = os.path.join('data', file_name)
     with urlopen(url) as file, open(local_path, 'wb') as f:
         f.write(file.read())
-    
-if __name__ == '__main__':
-    stock_symbols = ['GOOG', 'IBM', 'MSFT']
-    for stock_symbol in stock_symbols:
-        download_data(stock_symbol)
-    # for csv_file in get_csv_file('./data'):
-    #     write_csv(file_name, process_csv(os.path.join('data', csv_file)))
 
 def get_csv_files(dir_path):
     files_list = os.listdir(dir_path)
     csv_files = []
-    for file in csv_files:
-        if file.endwith('.csv'):
-            csv_files.append(file)
+    project_folder = os.path.abspath('')
+    for file in files_list:
+        if file.endswith('.csv'):
+            csv_files.append(os.path.join(project_folder, 'data', file))
     return csv_files
 
 def process_csv(file_name):
+    processed_data = []
     with open(file_name, 'r') as f:
         lines = f.readlines()
         header = lines.pop(0)
-        processed_data = []
-        header = header.strip().split(',')
-        header.append('Change')
-        processed_data.append(header)
+        processed_data.append(header.strip().split(',') + ['Change'])
         for line in lines:
             stock_data = line.strip().split(',')
             op, cp = float(stock_data[1]), float(stock_data[4])
@@ -46,18 +38,15 @@ def write_csv(file_name, write_data):
         for data in write_data:
             f.write(','.join(data)+'\n')
 
-# folder = 'data'   # your directory
-# files = [f for f in os.listdir(folder) if f.endswith('.csv')]
+if __name__ == '__main__':
+    stock_symbols = ['GOOG', 'IBM', 'MSFT']
+    for stock_symbol in stock_symbols:
+        download_data(stock_symbol)
+    for csv_file in get_csv_files('./data'):
+        write_csv(csv_file, process_csv(csv_file))
 
-file_name = './data/GOOG.csv'
-processed_data = process_csv(file_name)
-write_csv(file_name, processed_data)
+# get_csv_files('./data')
 
-file_name = './data/IBM.csv'
-processed_data = process_csv(file_name)
-write_csv(file_name, processed_data)
+# process_csv('C:/Users/HP/PycharmProjects/PythonHomework/data/MSFT.csv')
 
-file_name = './data/MSFT.csv'
-processed_data = process_csv(file_name)
-write_csv(file_name, processed_data)
 
